1edd2c724336821ce627e0999ce2104812e1a0cb
rayeberechi <faitheberechiraymond@gmail.com>
2024-10-08 11:07:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var b = 10;
 var c = 21;
 var d = 70;
 
-document.write(a + d)
+document.write((a + d) + "<br>")
 
-document.write((a + d)/a)
+document.write(((a + d)/a)+ "<br>")
 
-document.write(b * d)
+document.write((b * d)+ "<br>")
 
-document.write(d - c)
+document.write((d - c)+ "<br>")
+
+document.write(d / b)
